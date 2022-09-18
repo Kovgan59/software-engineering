5ffbb7ae68319982b88232b5979041a4f100fb4d
Kirill Kovgan <nesmogpridumatpochtu@gmail.com>
2022-09-18 22:01:41 +0500

    1_commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from MovAvg import MovAvg
+import numpy as np
+
+# x = np.array(100)
+# y = np.random.rand(100)
+x = [*range(1, 11, 1)]
+y = [*range(1, 11, 1)]
+
+data = MovAvg(x, y, 3)
+# data.draw_graph()
+data.draw_mov_avg_graph()
