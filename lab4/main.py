b8df2b52a733cc257042bbb8957c5226f59bfc3f
Kirill Kovgan <nesmogpridumatpochtu@gmail.com>
2022-12-21 17:32:20 +0500

    lab4

--- lab4/main.py
+++ lab4/main.py
@@ -1,26 +1,40 @@
-class A:
-    def __init__(self, a):
-        self.a = a
- 
-    # adding two objects
-    def __add__(self, o):
-        return self.a + o.a * 3
-ob1 = A(1)
-ob2 = A(2)
-ob3 = A("Geeks")
-ob4 = A("For")
- 
-print(ob1 + ob2)
-print(ob3 + ob4)
-# Actual working when Binary Operator is used.
-print(A.__add__(ob1 , ob2))
-print(A.__add__(ob3,ob4))
-#And can also be Understand as :
-print(ob1.__add__(ob2))
-print(ob3.__add__(ob4))
+class Clock:
+    __DAY = 86400   # число секунд в дне
+    def __init__(self, sec:int):
+        if not isinstance(sec, int):
+            raise ValueError("Введено не целое число")
 
-    # def___sub__
+        self.__sec = sec % self.__DAY
 
-    # def__mul__
-    
-    # def__truediv__
+    def getFormatTime(self):
+        s = self.__sec % 60         # секунды
+        m = (self.__sec // 60) % 60 # минуты
+        h = self.__sec // 3600 # часы
+        return f"{Clock.__getForm(h)}:{Clock.__getForm(m)}:{Clock.__getForm(s)}"
+
+    @staticmethod
+    def __getForm(x):
+        return str(x) if x > 9 else "0"+str(x)
+
+    def getSeconds(self):
+        return self.__sec 
+
+    def __add__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Необходим тип Clock!")
+        return Clock(self.__sec + other.getSeconds())
+
+    def __sub__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Необходим тип Clock!")
+        return Clock(self.__sec - other.getSeconds())
+
+c0 = Clock(86400)                  # весь день
+c1 = Clock(25200)                  # сон
+c2 = Clock(2000)                   # умыться + пожрать
+c3 = Clock(3200)                   # дорога до работы
+c4 = Clock(29000)                  # работа
+c5 = Clock(3200)                   # дорога до дома
+c6 = c0 - (c1 + c2 + c3 + c4 + c5) # остается времени до сна
+
+print(f"Времени до начала следующего дня: {c6.getFormatTime()}")   
